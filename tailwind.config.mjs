4289aa40cdb1c0c3380ebbd99b394b4632ad3176
Macias Carlos <maikispaiky85@gmail.com>
2024-03-13 16:34:00 -0600

    Feat: components

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.astro'],
+	theme: {
+		extend: {
+			colors: {
+				narang: "#A8B545",
+				morad: "#024554"
+			}
+		},
+	},
+	plugins: [],
+}
